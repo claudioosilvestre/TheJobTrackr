329e5f6fc01bc5def0a8be073c87c60848a626fa
Cláudio Silvestre <claudioosilvestre@hotmail.com>
2026-05-14 10:59:25 +0000

    add services and repository compnay impl

---
+++ TheJobTrackr/src/main/java/jobtrackr_api/repositories/CompanyRepository.java
@@ -0,0 +1,13 @@
+package jobtrackr_api.repositories;
+
+import jobtrackr_api.models.Company;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CompanyRepository extends JpaRepository<Company, Long> {
+
+    Optional<Company> findByName(String name);
+}
