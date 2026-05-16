09fb38d8906280532b0e547280e5a412a54791d2
Cláudio Silvestre <claudioo.silvestre96@gmail.com>
2026-05-16 08:54:12 +0100

    add jobApplication dtos

--- TheJobTrackr/src/main/java/jobtrackr_api/dtos/JobApplicationResponseDTO.java
+++ TheJobTrackr/src/main/java/jobtrackr_api/dtos/JobApplicationResponseDTO.java
@@ -1,4 +1,63 @@
 package jobtrackr_api.dtos;
 
+import jobtrackr_api.models.Company;
+import jobtrackr_api.models.JobStatus;
+import jobtrackr_api.models.User;
+
 public class JobApplicationResponseDTO {
+
+    private Long id;
+    private String position;
+    private JobStatus jobStatus;
+    private String notes;
+    private User user;
+    private Company company;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public JobStatus getJobStatus() {
+        return jobStatus;
+    }
+
+    public void setJobStatus(JobStatus jobStatus) {
+        this.jobStatus = jobStatus;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
 }
--- TheJobTrackr/src/main/java/jobtrackr_api/services/JobApplicationImpl.java
+++ TheJobTrackr/src/main/java/jobtrackr_api/services/JobApplicationImpl.java
@@ -1,4 +1,7 @@
 package jobtrackr_api.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class JobApplicationImpl {
 }
