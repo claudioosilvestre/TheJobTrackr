8f8c517c0131356a4e5e45776b799c4adc2edff3
Cláudio Silvestre <claudioosilvestre@hotmail.com>
2026-05-18 13:38:28 +0000

    update geminiController

--- TheJobTrackr/src/main/java/jobtrackr_api/controllers/GeminiController.java
+++ TheJobTrackr/src/main/java/jobtrackr_api/controllers/GeminiController.java
@@ -1,8 +1,10 @@
 package jobtrackr_api.controllers;
 
 import jakarta.validation.Valid;
-import jobtrackr_api.dtos.JobApplicationRequestDTO;
+import jobtrackr_api.dtos.AnalyzeRequestDTO;
+import jobtrackr_api.dtos.JobApplicationResponseDTO;
 import jobtrackr_api.services.GeminiService;
+import jobtrackr_api.services.JobApplicationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,14 +14,22 @@ import org.springframework.web.bind.annotation.*;
 public class GeminiController {
 
     private GeminiService geminiService;
-
+    private JobApplicationService jobApplicationService;
 
     @PostMapping("/{id}/analyze")
-    public ResponseEntity<String> analyzeJobCompatibility(@PathVariable Long id, @Valid @RequestBody JobApplicationRequestDTO jobApplicationRequestDTO) {
+    public ResponseEntity<String> analyzeJobCompatibility(@PathVariable Long id, @Valid @RequestBody AnalyzeRequestDTO analyzeRequestDTO) {
+        
+        JobApplicationResponseDTO jobApplicationResponseDTO  = jobApplicationService.findById(id);
+        
+        String result = geminiService.analyzeCompatibility(analyzeRequestDTO.getJobDescription(), jobApplicationResponseDTO.getPosition());
 
-        geminiService.analyzeCompatibility()
+        return ResponseEntity.ok(result);
     }
 
+    @Autowired
+    public void setJobApplicationService(JobApplicationService jobApplicationService) {
+        this.jobApplicationService = jobApplicationService;
+    }
 
     @Autowired
     public void setGeminiService(GeminiService geminiService) {
