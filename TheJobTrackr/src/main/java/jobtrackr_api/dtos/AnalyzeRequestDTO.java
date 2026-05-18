efe3b4bd310c13840982367937ddf4ba2760b676
Cláudio Silvestre <claudioosilvestre@hotmail.com>
2026-05-18 13:29:10 +0000

    add gemini controller incomplete

---
+++ TheJobTrackr/src/main/java/jobtrackr_api/dtos/AnalyzeRequestDTO.java
@@ -0,0 +1,14 @@
+package jobtrackr_api.dtos;
+
+public class AnalyzeRequestDTO {
+    
+    private String jobDescription;
+
+    public String getJobDescription() {
+        return jobDescription;
+    }
+
+    public void setJobDescription(String jobDescription) {
+        this.jobDescription = jobDescription;
+    }
+}
