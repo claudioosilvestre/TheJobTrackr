5f9cae5bf575f5eb306f0de426713baa852a9359
Cláudio Silvestre <claudioosilvestre@hotmail.com>
2026-06-15 08:21:17 +0100

    add getJobApplication shouldThrowException test

---
+++ TheJobTrackr/src/test/java/jobtrackr_api/controllers/JobApplicationControllerTest.java
@@ -0,0 +1,25 @@
+package jobtrackr_api.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class JobApplicationControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void getJobApplication_shouldThrowExceptionIfApplicationNotFound() throws Exception {
+
+        mockMvc.perform(get("/job-applications/999"))
+                .andExpect(status().isNotFound());
+    }
+}
